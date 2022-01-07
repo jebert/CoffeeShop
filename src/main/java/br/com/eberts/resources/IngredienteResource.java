5dbc519cd5882a5e02f28931ff0b9330dbc59085
Jebert <jonatan.ebert@gmail.com>
2022-01-07 03:38:23 -0300

    Paginação dos ingredientes

--- src/main/java/br/com/eberts/resources/IngredienteResource.java
+++ src/main/java/br/com/eberts/resources/IngredienteResource.java
@@ -5,11 +5,13 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -60,6 +62,19 @@ public class IngredienteResource {
 		iService.delete(id);
 		
 		return ResponseEntity.noContent().build();
+
+	}
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<Ingrediente>> findAll(
+			@RequestParam(value = "page", defaultValue = "0")  Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "3")Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String ordeBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+		
+		Page<Ingrediente> ing = iService.findPage(page,linesPerPage,ordeBy,direction);
+		
+		return ResponseEntity.ok().body(ing);
 
 	}
 
--- src/main/java/br/com/eberts/services/IngredienteService.java
+++ src/main/java/br/com/eberts/services/IngredienteService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.eberts.domain.Ingrediente;
@@ -50,6 +53,11 @@ public class IngredienteService {
 			throw new DataIntegrityException("Não é possivel excluir ingrediente usado em Produto existente!");
 		}
 		
+	}
+	
+	public Page<Ingrediente> findPage(Integer page,Integer linesPerPage, String ordeBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), ordeBy); 
+		return iRepo.findAll(pageRequest);
 	}
 
 }
